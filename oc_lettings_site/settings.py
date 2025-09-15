75d3c46d46dc0b241943de364b5eee23936dc034
Anne LS <anne2501@gmail.com>
2025-09-15 20:54:21 +0200

    Update ALLOWED_HOSTS to include production domain

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -36,7 +36,11 @@ SECRET_KEY = 'fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "*"]
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "projet-13-oc-lettings.onrender.com",
+]
 
 
 # Application definition
